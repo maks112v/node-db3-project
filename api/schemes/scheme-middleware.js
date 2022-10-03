214cdcb2756b092db5c46ed1ba080e66cef582c0
maks <11082252+maks112v@users.noreply.github.com>
2022-10-03 23:21:56 +0000

    added middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,6 @@
+const db = require('../../data/db-config');
+const yup = require('yup');
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +9,19 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const checkSchemeId = async (req, res, next) => {
+  const scheme = await db('schemes')
+    .where('scheme_id', req.params.scheme_id)
+    .first();
 
-}
+  if (!scheme) {
+    res.status(404).json({
+      message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+    });
+  } else {
+    next();
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,8 +32,12 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  if (yup.string().required().isValidSync(req.body.scheme_name)) {
+    res.status(400).json({ message: 'invalid scheme_name' });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,11 +49,23 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
-}
+  yup
+    .object()
+    .shape({
+      instructions: yup.string().required(),
+      step_number: yup.number().min(1).required(),
+    })
+    .validate(req.body, { abortEarly: false })
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      res.status(400).json({ message: 'invalid step' });
+    });
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
